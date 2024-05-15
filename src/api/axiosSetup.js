d6bbbef35c2ea480b973ab52965e9fc264d4e0fc
scottydixon <scotty.dixon@yahoo.com>
2024-05-15 18:40:04 -0500

    base url update

--- src/api/axiosSetup.js
+++ src/api/axiosSetup.js
@@ -1,15 +1,14 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8010/api"; // Your API base URL
+const BASE_URL = "https://win-the-day-server.vercel.app/";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
 });
 
-// Intercept request to add auth token
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token"); // Or wherever you store your token
+    const token = localStorage.getItem("token");
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
     }
